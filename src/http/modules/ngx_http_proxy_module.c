bd8619df7adf932bf010a7691d5c6630c0ed7b0a
Roman Arutyunyan <arut@nginx.com>
2024-11-15 08:23:53 +0400

    Upstream: early hints proxy.
    The change implements parsing early hints response from the proxied server.
    A new directive "proxy_pass_early_hints" allows passing early hints to the
    client.
    
    A new directive "early_hints" accepts a predicate to enable early hints for
    the client request.  By default, early hints are enabled for HTTP/2 and
    HTTP/3 and disabled for earlier HTTP versions due to protocol
    incompatibility in non-early-hints-aware clients.
    
    Example:
    
        # early_hints $arg_client_supports_early_hints;
    
        proxy_pass_early_hints on;
        proxy_pass http://bar.example.com;

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -470,6 +470,13 @@ static ngx_command_t  ngx_http_proxy_commands[] = {
       offsetof(ngx_http_proxy_loc_conf_t, upstream.pass_trailers),
       NULL },
 
+    { ngx_string("proxy_pass_early_hints"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      ngx_conf_set_flag_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_proxy_loc_conf_t, upstream.pass_early_hints),
+      NULL },
+
     { ngx_string("proxy_buffer_size"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
       ngx_conf_set_size_slot,
@@ -3594,6 +3601,7 @@ ngx_http_proxy_create_loc_conf(ngx_conf_t *cf)
     conf->upstream.pass_request_headers = NGX_CONF_UNSET;
     conf->upstream.pass_request_body = NGX_CONF_UNSET;
     conf->upstream.pass_trailers = NGX_CONF_UNSET;
+    conf->upstream.pass_early_hints = NGX_CONF_UNSET;
 
 #if (NGX_HTTP_CACHE)
     conf->upstream.cache = NGX_CONF_UNSET;
@@ -3939,6 +3947,9 @@ ngx_http_proxy_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 
     ngx_conf_merge_value(conf->upstream.pass_trailers,
                               prev->upstream.pass_trailers, 0);
+
+    ngx_conf_merge_value(conf->upstream.pass_early_hints,
+                              prev->upstream.pass_early_hints, 0);
 
     ngx_conf_merge_value(conf->upstream.intercept_errors,
                               prev->upstream.intercept_errors, 0);
